415f5415f3c885c067d83a957b87ec13991922f1
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-04-14 13:34:37 +0200

    work in progress

--- src/test/java/com/example/api/AbstractIntegrationTest.java
+++ src/test/java/com/example/api/AbstractIntegrationTest.java
@@ -3,7 +3,6 @@ package com.example.api;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -15,12 +14,6 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -48,26 +41,13 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
         registry.add("spring.datasource.username", postgresContainer::getUsername);
         registry.add("spring.datasource.password", postgresContainer::getPassword);
+        registry.add("spring.flyway.enabled", () -> true);
     }
 
 
     @BeforeAll
-    static void runFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(postgresContainer.getJdbcUrl(), postgresContainer.getUsername(), postgresContainer.getPassword()).load();
-
-        // Apply the migrations
-        flyway.migrate();
-    }
-
-    @Test
-    void testDatabaseIsMigrated() throws Exception {
-        try (Connection conn = DriverManager.getConnection(postgresContainer.getJdbcUrl(), postgresContainer.getUsername(), postgresContainer.getPassword())) {
-
-            ResultSet resultSet = conn.createStatement().executeQuery("SELECT COUNT(*) FROM information_schema.tables WHERE table_name = 'your_table_name'");
-            resultSet.next();
-            int count = resultSet.getInt(1);
-            assertEquals(1, count); // Adjust to match your expected state
-        }
+    static void migrate() {
+        Flyway.configure().dataSource(postgresContainer.getJdbcUrl(), postgresContainer.getUsername(), postgresContainer.getPassword()).load().migrate();
     }
 
 
